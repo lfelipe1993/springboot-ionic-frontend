617f45b8448d19a58f39001c3d1fb8c996d5650b
Luiz Felipe <lfelipe1993@live.com>
2021-02-16 19:17:02 -0300

    Subindo modificações

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://backend-springionic.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-lf.s3.amazonaws.com"
 }
